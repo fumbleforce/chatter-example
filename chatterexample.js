5d3aa236c2cfbba17548769679481b3a0916b9fd
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-03-29 15:57:09 +0200

    brought changes to branch

--- chatterexample.js
+++ chatterexample.js
@@ -1,6 +1,16 @@
 if (Meteor.isClient) {
 }
 
+if (Meteor.isServer) {
+  Accounts.onCreateUser(function(options, user) {
+    user.firstName = options.firstName;
+    user.lastName = options.lastName;
+    user.avatar = "http://localhost:3000/packages/jorgeer_chatter-semantic/public/images/avatar.jpg";
+    return user;
+  });
+}
+
 Chatter.configure({
-  nickProperty: "emails.0.address"
+  nickProperty: "emails.0.address",
+  avatarProperty: "avatar"
 });
