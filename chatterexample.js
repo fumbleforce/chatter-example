15af35cfc8534f2653a38427750fc64dedf6d7a8
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-04-11 09:33:48 +0200

    chatter aAPI now uses room id, when adding users to rooms

--- chatterexample.js
+++ chatterexample.js
@@ -1,16 +1,40 @@
+
+Chatter.configure({
+    nickProperty: "emails.0.address",
+    avatarProperty: "avatar"
+});
+
 if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Accounts.onCreateUser(function(options, user) {
-    user.firstName = options.firstName;
-    user.lastName = options.lastName;
-    user.avatar = "http://localhost:3000/packages/jorgeer_chatter-semantic/public/images/avatar.jpg";
-    return user;
-  });
+    Accounts.onCreateUser(function(options, user) {
+        user.firstName = options.firstName;
+        user.lastName = options.lastName;
+        user.avatar = "http://localhost:3000/packages/jorgeer_chatter-semantic/public/images/avatar.jpg";
+        return user;
+    });
+
+    const fun_and_gigggles = Chatter.addRoom("Fun and Giggles");
+    const dragons_and_dungeons = Chatter.addRoom("Dragons & Dungeons");
+    console.log(fun_and_gigggles);
+    console.log(dragons_and_dungeons);
+
+    const kristian = Meteor.users.findOne({_id: "H7zX4HJhqt9XYtPhN"});
+    const test = Meteor.users.findOne({_id: "G59qLzorwpDYQCfhR"});
+
+    if (kristian != undefined) {
+      const kristianId = kristian._id;
+      Chatter.addUser(kristianId, "admin");
+      Chatter.addUserToRoom(kristianId, fun_and_gigggles);
+      Chatter.addUserToRoom(kristianId, dragons_and_dungeons);
+
+    }
+
+    if (test != undefined) {
+      const testId = test._id;
+      Chatter.addUser(testId);
+      Chatter.addUserToRoom(testId, dragons_and_dungeons);
+    }
 }
 
-Chatter.configure({
-  nickProperty: "emails.0.address",
-  avatarProperty: "avatar"
-});
