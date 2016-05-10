7d540920f6ccdf66fa9d1d697167e04717e92148
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-05-10 23:30:16 +0200

    commented out setup code

--- chatterexample.js
+++ chatterexample.js
@@ -5,6 +5,7 @@ Chatter.configure({
 });
 
 if (Meteor.isClient) {
+
 }
 
 if (Meteor.isServer) {
@@ -14,5 +15,50 @@ if (Meteor.isServer) {
         user.avatar = "http://localhost:3000/packages/jorgeer_chatter-semantic/public/images/avatar.jpg";
         return user;
     });
- }
+
+    // const fun_and_gigggles = Chatter.addRoom({
+    //   name: "Fun and Giggles",
+    //   description: "A room to kid around"
+    // });
+    // const dragons_and_dungeons = Chatter.addRoom({
+    //   name: "Dragons & Dungeons",
+    //   description: "A world of fantasy"
+    // });
+    // const kristian = Meteor.users.findOne({_id: "H7zX4HJhqt9XYtPhN"});
+    // const test = Meteor.users.findOne({_id: "G59qLzorwpDYQCfhR"});
+
+    // if (kristian != undefined) {
+    //   const kristianId = kristian._id;
+
+    //   Chatter.addUser({
+    //     userId: kristianId,
+    //     userType: "admin"
+    //   });
+
+    //   Chatter.addUserToRoom({
+    //     userId: kristianId,
+    //     roomId: fun_and_gigggles
+    //   });
+
+    //   Chatter.addUserToRoom({
+    //     userId: kristianId,
+    //     roomId: dragons_and_dungeons
+    //   });
+
+    // }
+    // if (test != undefined) {
+    //   const testId = test._id;
+
+    //   Chatter.addUser({
+    //     userId: testId
+    //   }, function(error, res) {
+    //     console.log(error);
+    //   });
+
+    //   Chatter.addUserToRoom({
+    //     userId: testId,
+    //     roomId: dragons_and_dungeons
+    //   });
+    // }
+}
 
